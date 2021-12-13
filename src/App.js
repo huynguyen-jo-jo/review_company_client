b4df78c0d47fe374f9d375602a0888e98b030e7c
huynguyen-jo-jo <73032228+huynguyen-jo-jo@users.noreply.github.com>
2021-12-13 19:09:24 +0700

    add Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
-import "./App.css";
-// import Login from "./components/login/login";
-import HomePages from "./components/homePage/homePage.js";
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import './App.css';
+import Desktop3 from './Componant/Desktop3';
+import Desktop7 from './Componant/Desktop7';
+import Desktop1 from './Componant/Desktop1/Desktop1'
+import HomePages from './Componant/home_Pages/home_Pages';
+import Login from './Componant/login/login';
 
 function App() {
   return (
-    <div>
-      <HomePages />
-      {/* <Login /> */}
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Desktop3/>} path="/desktop-3"/>
+          <Route element={<Desktop7/>} path="/desktop-7"/>
+          <Route element={<Desktop1/>} path="/desktop-1"/>
+          <Route element={<HomePages/>} path="/HomePages"/>
+          <Route element={<Login/>} path="/Login"/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
